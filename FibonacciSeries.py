88338d93724700c40b7ff7d160e3790afc0fbac7
praneethpeddi <56199249+praneethpeddi@users.noreply.github.com>
2020-09-29 20:09:15 +0530

    Add files via upload

---
+++ FibonacciSeries.py
@@ -0,0 +1,10 @@
+n = int(input("Enter N value: "))
+a = 0
+b = 1
+print(a)
+print(b)
+for i in range(2, n):
+    c = a + b
+    a = b
+    b = c
+    print(c)
---
+++ Pattern3.10.py
@@ -0,0 +1,5 @@
+n = 6
+for i in range(n):
+    for j in range(1, n-i):
+        print(j, end='')
+    print()
