2aec1993a73975ee06f872eb0253d98720167289
praneethpeddi <56199249+praneethpeddi@users.noreply.github.com>
2020-09-29 14:15:05 +0530

    Add files via upload

---
+++ PassFail.py
@@ -0,0 +1,7 @@
+S1 = int(input("Enter the Marks for S1: "))
+S2 = int(input("Enter the Marks for S2: "))
+S3 = int(input("Enter the Marks for S3: "))
+if S1 >= 35 and S2 >= 35 and S3 >= 35:
+    print("Promoted")
+else:
+    print("Not Promoted")
