f854adb83fbb4e9843a6e0349f4f67f7cf2070c2
praneethpeddi <56199249+praneethpeddi@users.noreply.github.com>
2020-09-30 08:06:21 +0530

    Update FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,10 +1,10 @@
 n = int(input("Enter N value: "))
 a = 0
 b = 1
-print(a)
-print(b)
+SumofTerms = 1
 for i in range(2, n):
     c = a + b
     a = b
     b = c
-    print(c)
+    SumofTerms = SumofTerms + c
+print(SumofTerms)
