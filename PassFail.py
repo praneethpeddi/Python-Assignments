b276ca5b23cf1e84181ed182f51e233f01672e7b
praneethpeddi <56199249+praneethpeddi@users.noreply.github.com>
2020-09-30 08:33:39 +0530

    Update PassFail.py

--- PassFail.py
+++ PassFail.py
@@ -1,7 +1,13 @@
-S1 = int(input("Enter the Marks for S1: "))
-S2 = int(input("Enter the Marks for S2: "))
-S3 = int(input("Enter the Marks for S3: "))
-if S1 >= 35 and S2 >= 35 and S3 >= 35:
+MarksList = []
+for i in range(3):
+    S = int(input("Enter the Marks for the Subjects: "))
+    MarksList.append(S)
+    for Marks in MarksList:
+        if Marks >= 35:
+            Pass = 1
+        else:
+            Pass = 0
+if Pass == 1:
     print("Promoted")
 else:
     print("Not Promoted")
