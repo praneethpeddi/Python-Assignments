9cb655c88f5813557f94350b5c1e564a12f773eb
praneethpeddi <56199249+praneethpeddi@users.noreply.github.com>
2020-10-01 14:05:16 +0530

    Update 3.12.Pattern.py

--- 3.12.Pattern.py
+++ 3.12.Pattern.py
@@ -1,5 +1,6 @@
-n = 5
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end=' ')
-    print('')
+n = 1
+while n <= 5:
+    for i in range(1, n+1):
+        print(i, end='')
+    print()
+    n = n + 1
