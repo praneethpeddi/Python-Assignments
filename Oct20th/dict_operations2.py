f874fdacd94749d2c31c6f785f96a9041639d346
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-10-21 15:15:05 +0530

    modified

--- Oct20th/dict_operations2.py
+++ Oct20th/dict_operations2.py
@@ -1,29 +1,31 @@
 # from CaseInsensitiveDict import *
 
-alpha = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'xyz': [24, 25, 26]}
-
 def get_key_by_value(*args):
     for key, val in list(alpha.items()):
         for i in args:
             if val == i:
-                print(f"{key} is key of {val}")
-				
+                print(f"{key} is key to value {val}")
+
+
 def get_value_by_case_insensitive_key(keys):
     for key, val in list(alpha.items()):
         if keys.lower() == key:
             print(f"value of the {keys}                  :   {val}")
 
+
 def del_item(keys):
     for key, val in list(alpha.items()):
         if keys.lower() == key:
             del alpha[key]
-            print(f"dictionary after deleting key      :   {alpha}")
+            print(f"dictionary after deleting key     :   {alpha}")
+
 
 def pop_element(keys):
     for key, val in list(alpha.items()):
         if keys.lower() == key:
-            alpha.pop(key)
-            print(f"dictionary after deleting the key   :   {alpha}")
+            alpha.pop(keys)
+            print(f"dictionary after deleting key     :   {alpha}")
+
 
 def main():
     get_key_by_value(1, 2, 3, 4)
@@ -31,6 +33,8 @@ def main():
     del_item('D')
     pop_element('a')
 
+
 if __name__ == "__main__":
-    print(dir(dict))
+    alpha = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'xyz': [24, 25, 26]}
+    print(f"Original Dictionary                :   {alpha}")
     main()
