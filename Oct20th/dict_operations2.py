8c7f6813b936f7ab9c169fe446fd3a50958504f6
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-10-21 14:40:36 +0530

    modified

--- Oct20th/dict_operations2.py
+++ Oct20th/dict_operations2.py
@@ -2,19 +2,16 @@
 
 alpha = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'xyz': [24, 25, 26]}
 
-
 def get_key_by_value(*args):
     for key, val in list(alpha.items()):
         for i in args:
             if val == i:
                 print(f"{key} is key of {val}")
-
-
+				
 def get_value_by_case_insensitive_key(keys):
     for key, val in list(alpha.items()):
         if keys.lower() == key:
             print(f"value of the {keys}                  :   {val}")
-
 
 def del_item(keys):
     for key, val in list(alpha.items()):
@@ -22,20 +19,17 @@ def del_item(keys):
             del alpha[key]
             print(f"dictionary after deleting key      :   {alpha}")
 
-
 def pop_element(keys):
     for key, val in list(alpha.items()):
         if keys.lower() == key:
-            alpha.pop(keys)
+            alpha.pop(key)
             print(f"dictionary after deleting the key   :   {alpha}")
-
 
 def main():
     get_key_by_value(1, 2, 3, 4)
     get_value_by_case_insensitive_key('XyZ')
     del_item('D')
     pop_element('a')
-
 
 if __name__ == "__main__":
     print(dir(dict))
