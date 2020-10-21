97845ec98e84585d0c65a7eb3d507b99322f9533
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-10-21 23:11:53 +0530

    modified

--- Oct20th-Dictionaries/diff_copy_assignment.py
+++ Oct20th-Dictionaries/diff_copy_assignment.py
@@ -4,7 +4,9 @@ dict1 = {1: 'one', 2: 'two', 3: 'three', 4: 'four'}
 dict2 = dict1
 dict2.clear()
 print(dict1)
+print(type(dict1))
 print(dict2)
+print(type(dict2))
 print()
 
 # assigning using the copy method
@@ -13,4 +15,6 @@ dict1 = {1: 'one', 2: 'two', 3: 'three', 4: 'four'}
 dict2 = dict1.copy()
 dict2.clear()
 print(dict1)
+print(type(dict1))
 print(dict2)
+print(type(dict2))
