06d2916b07218e6223a9513b3149256b8771b770
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-10-26 00:18:23 +0530

    modified

--- Oct24th-Selenium/sample.py
+++ Oct24th-Selenium/sample.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome(r"C:\Users\user\AppData\Roaming\Python\Python37\site-packages\chromedriver_win32"
                           r"\chromedriver.exe")
 driver.get("https://www.google.com")
+time.sleep(5)
 driver.find_element_by_name('q').send_keys("Python")
+time.sleep(3)
 driver.maximize_window()
 driver.find_element_by_name('btnK').click()
+time.sleep(5)
 driver.get("https://www.youtube.com")
-driver.find_element_by_id("search").send_keys("Python tutorial")
+time.sleep(5)
+driver.find_element_by_name("search_query").send_keys("Python tutorial")
+driver.find_element_by_id('search-icon-legacy').click()
+time.sleep(5)
 driver.minimize_window()
+driver.close()
