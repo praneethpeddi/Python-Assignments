512b13e50b553667d47dc125c0254d35403d0b1b
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-10-31 06:38:03 +0530

    modified

--- Oct27th-web-scrapping/get_urls_selenium.py
+++ Oct27th-web-scrapping/get_urls_selenium.py
@@ -8,12 +8,12 @@ def initialize_driver():
         if env == 'win32':
             path = r'C:\Users\user\PycharmProjects\selenium_framework\drivers\chromedriver.exe'
             driver = webdriver.Chrome(path)
-            print(f" path : {path} found, chrome driver is initialized")
+            print(f"path : {path} found, chrome driver is initialized")
             return driver
         else:
-            path = '/path/to/driver/chromedriver'
-            driver = webdriver.Chrome(executable_path=path)
-            print(f" path : {path} found, chrome driver is initialized")
+            path = '/usr/local/bin/chromedriver'
+            driver = webdriver.Chrome(path)
+            print(f"path : {path} found, chrome driver is initialized")
             return driver
     except Exception as err:
         print(f"{err} : not found, chrome driver is not initialized")
@@ -22,20 +22,24 @@ def initialize_driver():
 def launch_homepage(driver, url):
     try:
         home_page = driver.get(url)
+        driver.maximize_window()
         print(f'{url} : url launched')
     except Exception as err:
         print(f'{url} : url not launched')
 
 
-def launch_subpages(driver, text, url):
+def launch_subpages_and_get_html(driver, text, url):
     try:
         for partial_text in text:
             link = driver.find_elements_by_partial_link_text(partial_text)
             link[0].click()
+            print(f"Partial link text found, clicked on the link containing {partial_text}")
+            html = driver.page_source
+            print(html)
             home_page = driver.get(url)
-            print(f" partial link text found, clicked on the link containing {partial_text}")
+            print(f'{url} : url launched')
     except Exception as err:
-        print(f" {err} : partial link text not found, did not clicked on the link containing {partial_text}")
+        print(f" {err} : Partial link text not found, did not clicked on the link containing {partial_text}")
 
 
 def main():
@@ -43,7 +47,7 @@ def main():
     text = ['Key generation', 'Fellow Members', 'already test', 'no longer supported', 'survey']
     driver = initialize_driver()
     launch_homepage(driver, url)
-    launch_subpages(driver, text, url)
+    launch_subpages_and_get_html(driver, text, url)
 
 
 if __name__ == '__main__':
