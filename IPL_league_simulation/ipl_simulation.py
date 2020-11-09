be49a6da6a8603f36910b32da263dedd4986b15c
praneethpeddi1995@gmail.com <praneethpeddi1995@gmail.com>
2020-11-09 06:33:56 +0530

    modified

--- IPL_league_simulation/ipl_simulation.py
+++ IPL_league_simulation/ipl_simulation.py
@@ -1,12 +1,14 @@
 import random
 import operator
+import time
+from termcolor import colored
 
 
 def generate_teams(n):
     team_list = []
     for i in range(1, n + 1):
         team_list.append('Team' + str(i))
-    print(f"Total teams participating in the IPL : {team_list}")
+    print(f"Total teams participating in the IPL                    : {team_list}")
     return team_list
 
 
@@ -17,8 +19,8 @@ def league_matches(teams):
             matches = (teams[i], teams[len(teams) - j - 1])
             if teams[i] != teams[len(teams) - j - 1]:
                 set_of_matches.append(matches)
-    print(f"Total set of league matches between each team : {set_of_matches}")
-    print(f"Total number of league matches : {len(set_of_matches)}")
+    print(f"Total set of league matches between each team           : {set_of_matches}")
+    print(f"Total number of league matches                          : {len(set_of_matches)}")
     return set_of_matches
 
 
@@ -26,43 +28,45 @@ def match_between_teams(teams, matches):
     total_score_each_team = {}
     for each_team in teams:
         total_score_each_team[each_team] = 0
-    print(f"Total score of each team before league matches : {total_score_each_team}")
+    print(f"Total score of each team before league matches          : {total_score_each_team}")
     for each_match in matches:
         res_each_match = random.choice(each_match)
-        print(f"Winner of the match {each_match} : {res_each_match}")
+        print(f"Winner of the match {each_match}                  : {res_each_match}")
+        # time.sleep(1)
         total_score_each_team[res_each_match] += 2
-    print(f"Total score of each team after league matches : {total_score_each_team}")
+    print(f"Total score of each team after league matches           : {total_score_each_team}")
     return total_score_each_team
 
 
 def qualify_playoffs(total_score_each_team):
     playoff_teams = dict(sorted(total_score_each_team.items(), key=operator.itemgetter(1), reverse=True))
-    print(f"Score of each team after sorting in descending order {playoff_teams}")
+    print(f"Score of each team after sorting in descending order    : {playoff_teams}")
     for i in range(4):
         playoff_teams.popitem()
-    print(f"Total number of teams qualified for playoffs : {playoff_teams}")
+    print(f"Total number of teams qualified for playoffs            : {playoff_teams}")
     return playoff_teams
 
 
 def playoff_matches(playoff_teams):
     lis = [(k, v) for k, v in playoff_teams.items()]
     playoff1 = random.choice((lis[0][0], lis[1][0]))
+    print(f"Team won in the playoff1, Finalist1                     : {playoff1}")
+    playoff2 = random.choice((lis[2][0], lis[3][0]))
+    print(f"Team won in the playoff2                                : {playoff2}")
     if playoff1 == (lis[0][0]):
         playoff1_looser = (lis[1][0])
     else:
         playoff1_looser = (lis[0][0])
-    playoff2 = random.choice((lis[2][0], lis[3][0]))
+    print(f"Team lost in the playoff1                               : {playoff1_looser}")
     playoff3 = random.choice((playoff2, playoff1_looser))
-    print(f"Team won in the playoff1, Finalist1 : {playoff1}")
-    print(f"Team won in the playoff2 : {playoff2}")
-    print(f"Team lost in the playoff1 : {playoff1_looser}")
-    print(f"Team lost in the playoff3, Finalist2 : {playoff3}")
+    print(f"Team won in the playoff3, Finalist2                     : {playoff3}")
     return playoff1, playoff3
 
 
 def final_match(finalist1, finalist2):
     winner = random.choice((finalist1, finalist2))
-    return winner
+    print(f"Congrats to " + colored("\033[1m" + f'{winner}' + "\033[0m", 'green') + " for winning the IPL")
+    # print(colored("\033[1m" + f'{winner}' + "\033[0m" , 'green'))
 
 
 def main():
@@ -72,8 +76,7 @@ def main():
     total_score_each_team = match_between_teams(teams, matches)
     playoff_teams = qualify_playoffs(total_score_each_team)
     finalist1, finalist2 = playoff_matches(playoff_teams)
-    winner = final_match(finalist1, finalist2)
-    print(f"Congratulations to the team {winner} for winning the league")
+    final_match(finalist1, finalist2)
 
 
 if __name__ == '__main__':
